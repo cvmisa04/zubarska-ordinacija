0b2444d6a823be536acfabacb9092019f0d7e69a
Milos Cvetkovic <cvmisa04@gmail.com>
2021-09-03 19:04:43 +0200

    Intial commit

--- server/server.js
+++ server/server.js
@@ -32,10 +32,10 @@ app.use(morgan("dev"));
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"./client/build")))
+    app.use(express.static(path.join(__dirname,"/../../client/build")))
     app.get('*',(req,res)=>{
 
-        res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "/../../client/build", "index.html"));
 
     })
 
