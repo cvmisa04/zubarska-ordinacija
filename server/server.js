89c8aa5ec976fe665e0e7c9d732e17b87b03f0fe
Milos Cvetkovic <cvmisa04@gmail.com>
2021-09-03 19:31:44 +0200

    Intial commit

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,"../client/build")))
     app.get('*',(req,res)=>{
 
-        res.sendFile(express.static(path.resolve(__dirname, "../client/build/index.html")));
+        //res.sendFile(express.static(path.resolve(__dirname, "../client/build/index.html")));
+        res.sendFile('index.html', { root: path.join(__dirname, '../client/build') });
 
     })
 
